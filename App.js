c331c380cb22f882bb75e29a5766c18258863439
kwan19482 <kwan5743@gmail.com>
2021-12-22 11:47:03 +0700

    Webview and Stack Navigation

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import AlertExample from './components/AlertExample'
-import ModelExample from './components/ModelExample'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <ModelExample/>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} option={{ title: 'หน้าหลัก' }} />
+        <Stack.Screen name="About" component={AboutScreen} option={{ title: 'เกี่ยวกับเรา' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
